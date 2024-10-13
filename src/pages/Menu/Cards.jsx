600b86c87a9246626c2e95eb51e71d3fc170bf2d
Akhalid <86753011+akhalid07@users.noreply.github.com>
2024-10-13 16:59:21 -0400

    Create Cards.jsx for menu

---
+++ src/pages/Menu/Cards.jsx
@@ -0,0 +1,20 @@
+import { Card, Image, Group } from '@mantine/core'
+
+function Demo({ name, description, price, id }) {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder className="flex-row-reverse-container card-container">
+      <Card.Section>
+        <Image src={`/${id}.png`} height={100} alt="Norway" />
+      </Card.Section>{' '}
+      <div className="column-center">
+        <Group justify="space-between" mt="md" mb="xs">
+          <h3 className="menu-title">{name}</h3>
+        </Group>
+        <p className="menu-description">{description}</p>
+        <p className="menu-price">Price: ${price}</p>{' '}
+      </div>
+    </Card>
+  )
+}
+
+export default Demo

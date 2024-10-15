ee52dc9bbd4f22ecc1f33251483d660ba59dccf2
“Kevin-JHG” <kjhernandezgaspar@my.waketech.edu>
2024-10-15 19:04:10 -0400

    - Access Porfile Viewing/Editing - Profile Icon - Final Landing page layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,34 +1,56 @@
-import { Outlet, NavLink } from 'react-router-dom'
+import { Outlet, NavLink, useNavigate } from 'react-router-dom';
+import './layout.css';
 
-import './layout.css'
+// Prevent navigation link dragging
+const DraggableNavLink = ({ to, children }) => (
+  <NavLink draggable="false" to={to}>
+    {children}
+  </NavLink>
+);
 
 export const Layout = () => {
+  const navigate = useNavigate();
+
+  const handleProfileClick = () => {
+    navigate('/profile');
+  };
+
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/reservations">Reservations</NavLink>
-          </li>
-          <li>
-            <NavLink to="/menu">Menu</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-          <li>
-            <NavLink to="/register">Register</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
+        <div className="nav-container">
+          <ul>
+            <li>
+              <DraggableNavLink to="/">Home</DraggableNavLink>
+            </li>
+            <li>
+              <DraggableNavLink to="/reservations">Reservations</DraggableNavLink>
+            </li>
+            <li>
+              <DraggableNavLink to="/menu">Menu</DraggableNavLink>
+            </li>
+            <li>
+              <DraggableNavLink to="/login">Login</DraggableNavLink>
+            </li>
+            <li>
+              <DraggableNavLink to="/register">Register</DraggableNavLink>
+            </li>
+            <li>
+              <DraggableNavLink to="/contact">Contact</DraggableNavLink>
+            </li>
+          </ul>
+        </div>
+        
+        <div className="profile-icon" onClick={handleProfileClick}>
+          <img 
+            src="./profile_avatar.png" 
+            alt="Profile" 
+            className="profile-icon-img"
+          />
+        </div>
       </nav>
 
       <Outlet />
     </>
-  )
-}
+  );
+};
